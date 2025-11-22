171f3187d130459d1d58327a53330988fd297ac1
psuarez <pauladzuck@hotmail.com>
2025-11-22 15:39:13 -0600

    updates again

--- app/routes/home.tsx
+++ app/routes/home.tsx
@@ -3,7 +3,7 @@ import { Welcome } from "../welcome/welcome";
 
 export function meta({}: Route.MetaArgs) {
   return [
-    { title: "New React Router App" },
+    { title: "Welcome!" },
     { name: "description", content: "Welcome to React Router!" },
   ];
 }
