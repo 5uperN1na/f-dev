0e292513f581063ae4da22251e997c36b473df81
psuarez <pauladzuck@hotmail.com>
2025-12-04 20:17:39 -0600

    Adding hero componenet

--- app/routes/home/index.tsx
+++ app/routes/home/index.tsx
@@ -1,4 +1,5 @@
 import type { Route } from "./+types/index";
+import Hero from "~/components/Hero";
 
 export function meta({}: Route.MetaArgs) {
   return [
@@ -8,8 +9,9 @@ export function meta({}: Route.MetaArgs) {
 }
 
 export default function Home() {
-   console.log('test')
-  return <section>
-   
-    My App</section>;
+  return (
+    <section>
+      <Hero  name='Paula'/>
+    </section>
+  );
 }
