b217e1cf6441d90ec675b0525b34137b01c5180e
psuarez <pauladzuck@hotmail.com>
2025-12-15 20:37:47 -0600

    Adding filtering to projects

--- app/routes/projects/index.tsx
+++ app/routes/projects/index.tsx
@@ -14,28 +14,62 @@ export async function loader({
 }
 
 const ProjectsPage = ({ loaderData }: Route.ComponentProps) => {
-  const { projects } = loaderData as { projects: Project[] };
-  // console.log(projects);
+  const [selectedCategory, setSelectedCategory] = useState("All");
 
   const [currentPage, setCurrentPage] = useState(1);
   const projectsPerPage = 10;
 
+  const { projects } = loaderData as { projects: Project[] };
+
+  //get unique categories
+
+  const categories = [
+    "All",
+    ...new Set(projects.map((project) => project.category)),
+  ];
+
+  //filter project based on the category
+  const filteredProjects =
+    selectedCategory === "All"
+      ? projects
+      : projects.filter((project) => project.category === selectedCategory);
+
   //calculate total pages
-  const totalPages = Math.ceil(projects.length / projectsPerPage);
+  const totalPages = Math.ceil(filteredProjects.length / projectsPerPage);
 
   //get current page
   const indexOfLast = currentPage * projectsPerPage;
   const indexOfFirst = indexOfLast - projectsPerPage;
-  const currentProjects = projects.slice(indexOfFirst, indexOfLast);
+  const currentProjects = filteredProjects.slice(indexOfFirst, indexOfLast);
 
   return (
     <>
-      <h2 className="text-3xl font-bold mb-8 ">🚀Projects</h2>;
+      <h2 className="text-3xl font-bold mb-8 ">🚀Projects</h2>
+
+      <div className="flex flex-wrap gap-2 mb-8">
+        {categories.map((category) => (
+          <button
+            key={category}
+            onClick={() => {
+              setSelectedCategory(category);
+              setCurrentPage(1);
+            }}
+            className={`px-3 py-1 rounded text-sm cursor-pointer ${selectedCategory === category
+                ? "bg-blue-600 text-white"
+                : "bg-gray-700 text-gray-200"
+            }`}
+          >
+            {category}
+          </button>
+        ))}
+      </div>
+
       <div className="grid gap-6 sm:grid-cols-2">
         {currentProjects.map((project) => (
           <ProjectCard key={project.id} project={project} />
         ))}
       </div>
+
       <Pagination
         totalPages={totalPages}
         currentPage={currentPage}
